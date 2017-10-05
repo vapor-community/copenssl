62bf727c68e6c348a31be8da3baf0d2fe6c355cd
Joannis Orlandos <joannis@orlandos.nl>
2017-10-05 20:57:04 +0200

    rename CTLS to COpenSSL

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -4,6 +4,9 @@ import PackageDescription
 let package = Package(
     name: "COpenSSL",
     pkgConfig: "openssl",
+    providers: [
+        .apt(["openssl libssl-dev"])
+    ],
     products: [
         .library(name: "COpenSSL", targets: ["COpenSSL"])
     ],
